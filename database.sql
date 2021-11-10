aef1177c238660eec631abc21e0a80a4106272f9
RamonJales <ramonjales123@gmail.com>
2021-11-10 16:23:17 -0300

    database.sql file created

---
+++ database.sql
@@ -0,0 +1,8 @@
+CREATE TABLE student (
+    Id int() NOT NULL AUTO_INCREMENT,
+    Name varchar(30) NOT NULL,
+    EMAIL varchar(100) NOT NULL,
+    Street varchar(100) NOT NULL
+    BirthDate date NOT NULL,
+    PRIMARY KEY (Id)
+);
